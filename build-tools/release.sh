82410937af8ac0767dabfa189b56ba0fd54c41b3
Osman Ungur <osmanungur@gmail.com>
2017-10-24 21:26:56 +0200

    Improve inline documentation

--- build-tools/release.sh
+++ build-tools/release.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-# This script requires curl, git and jq (https://stedolan.github.io/jq/) in PATH.
+
+# This script requires curl, git, jekyll (requires ruby, ruby-dev, build-essential (for make and gcc))
+# and jq (https://stedolan.github.io/jq/) in PATH
+# TODO: Logging? 
 
 set -euo pipefail
 # exit on non zero exit code, fail fast when variable not found, fail immediately when a command fails behind pipe
+#
+# Using this flags helps to ensure all commands returns 0 and also checks variables strictly
+# If any build step fails, the script will be terminated
 
 set -o xtrace
 # ahem, debug mode
 
+# constants
 readonly current_sha_file='/tmp/yurtdisinda-calismak-current-sha'
 readonly repository_url='https://github.com/o/yurtdisinda-calismak-wiki.git'
 readonly repository_commits_url='https://api.github.com/repos/o/yurtdisinda-calismak-wiki/commits'
@@ -19,9 +26,11 @@ touch "${current_sha_file}"
 mkdir -p "${build_folder}"
 mkdir -p "${source_folder}"
 
+# Get latest commit SHA using Github API and Jq
 readonly latest_sha="$(curl ${repository_commits_url} | jq --raw-output '.[0].sha')"
 readonly current_sha="$(cat ${current_sha_file})"
 
+# Compare current SHA with latest commit from upstream
 if [ "${latest_sha}" = "${current_sha}" ]
 then
     echo 'Already up-to-date'
@@ -33,8 +42,11 @@ else
     mkdir -p "${content_folder}"
 
     git clone "${repository_url}" "${content_folder}"
+    # Create static version of content
     jekyll build --source "${content_folder}" --destination="${destination_folder}"
+    # Force symlink new build and don't follow current symlink
     ln -nsf "${destination_folder}" "${web_folder}"
+    # Update latest commit SHA
     echo "${latest_sha}" > "${current_sha_file}"
 fi
 
